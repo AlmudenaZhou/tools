d3474d1bacea6bb4fce5adaa6f341f8c9786ee40
Almudena <almu180@gmail.com>
2023-02-11 11:51:57 +0100

    [FEATURE] Added different NA representations

--- extend_pandas/handle_missing_data.py
+++ extend_pandas/handle_missing_data.py
@@ -18,9 +18,19 @@ class MissingMethods:
         self._obj = pandas_obj
         self.missing_value_representation_by_type = {
             'numerical': [0, -1, 99, 999, -99, -999],
-            'categorical': ['-', 'na', 'n/a', 'null', 'nil', 'empty', ''],
-            'datetime': ['1970-01-01', '1900-01-01, 2099-12-31', '2999-12-31', '9999-12-31']
+            'categorical': ['-', 'na', 'n/a', 'null', 'nil', 'empty', '', '#n/d'],
+            'datetime': ['1970-01-01', '1900-01-01', '2099-12-31', '2999-12-31', '9999-12-31']
         }
+        self._all_missing_value_repr = self._calc_all_possible_na_combinations()
+
+    def _calc_all_possible_na_combinations(self):
+        string_numeric_na = [str(numeric_na) for numeric_na in self.missing_value_representation_by_type['numerical']]
+        upper_string_na = [string_na.upper() for string_na in self.missing_value_representation_by_type['categorical']]
+        cap_string_na = [str_na.capitalize() for str_na in self.missing_value_representation_by_type['categorical']]
+        datetime_na = [datetime.strptime(dt_na, '%Y-%m-%d')
+                       for dt_na in self.missing_value_representation_by_type['datetime']]
+        all_original_na = [value for values in self.missing_value_representation_by_type.values() for value in values]
+        return string_numeric_na + upper_string_na + cap_string_na + datetime_na + all_original_na
 
     def number_missing(self) -> int:
         """
