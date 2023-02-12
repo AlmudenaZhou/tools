74bef0d5fd081914ef60f781f138b347a8faf279
AlmudenaZhou <almudena.zhou.rl@gmail.com>
2023-02-12 18:38:49 +0100

    [REFACTOR] Missing_suffix as input of correlation_info and added type hints to correlation_info

--- statistics/correlations.py
+++ statistics/correlations.py
@@ -6,9 +6,9 @@ def correlation_pointbiserialr(x, y):
     return stats.pointbiserialr(x, y).correlation
 
 
-def correlation_info(df, corr_method='pearson', corr_threshold=0.8):
+def correlation_info(df, corr_method: str = 'pearson', corr_threshold: float = 0.8, missing_suffix: str = '_NA'):
     all_corr = df.corr(method=corr_method)
-    corr_missing_data = all_corr[all_corr.index.str.endswith('_is_missing')]
+    corr_missing_data = all_corr[all_corr.index.str.endswith(missing_suffix)]
     corr_missing_data.replace(1.0, np.nan, inplace=True)
     return (corr_missing_data[corr_missing_data.abs() > corr_threshold]
             .dropna(axis=1, how='all')
