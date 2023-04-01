1e3381b4250d964b51b083c1f0a8a6af9ae74e84
AlmudenaZhou <almudena.zhou.rl@gmail.com>
2023-04-01 21:04:56 +0200

    [FEATURE] Added categorical specific transformers using the base ones

---
+++ transformations/categorical.py
@@ -0,0 +1,79 @@
+import numpy as np
+from sklearn.preprocessing import OneHotEncoder, OrdinalEncoder
+
+from tools.transformations.base import SklearnTargetedTransformer
+
+
+class OneHotTargetedTransformer(SklearnTargetedTransformer):
+
+    def __init__(self, columns_to_apply, **model_kwargs):
+        super().__init__(columns_to_apply, **model_kwargs)
+
+    def fit(self, x, y=None):
+        return super().fit(x)
+
+    def _individual_fit(self, x, feature):
+        return OneHotEncoder(sparse=False, **self._model_kwargs).fit(x[[feature]])
+
+    def transform(self, x, y=None):
+        return super().transform(x)
+
+    def _individual_transform(self, x, feature):
+        self._array_columns.extend(self._encoders[feature].categories_[0])
+        return self._encoders[feature].transform(x[[feature]])
+
+
+class OrdinalTargetedTransformer(SklearnTargetedTransformer):
+
+    def __init__(self, ordered_labels, columns_to_apply=None, **model_kwargs):
+        super().__init__(columns_to_apply, **model_kwargs)
+        self._ordered_labels = ordered_labels
+
+    def fit(self, x, y=None):
+        return super().fit(x)
+
+    def _individual_fit(self, x, feature):
+        """
+        Issue: handle_unknown: 'use_encoded_value' is not properly working and throws an error
+        """
+        if self._model_kwargs['handle_unknown'] == 'use_encoded_value':
+            return self._fit_model_with_nans(x, feature)
+        else:
+            return self._fit_model(x, feature)
+
+    def _fit_model(self, x, feature):
+        return OrdinalEncoder(categories=[self._ordered_labels[feature]],
+                              **self._model_kwargs).fit(x[[feature]])
+
+    def _fit_model_with_nans(self, x, feature):
+        x_filtered = x.loc[x[feature].notna()]
+        return self._fit_model(x_filtered, feature)
+
+    def transform(self, x, y=None):
+        return super().transform(x)
+
+    def _individual_transform(self, x, feature):
+        """
+        Since the handle unknown is broken in transform, I will bypass it
+        """
+        self._array_columns.append(feature)
+        if self._model_kwargs['handle_unknown'] == 'use_encoded_value':
+            return self._transform_data_with_nans(x, feature)
+        else:
+            return self._transform_data(x, feature)
+
+    def _transform_data(self, x, feature):
+        return self._encoders[feature].transform(x[[feature]])
+
+    def _transform_data_with_nans(self, x, feature):
+        """
+        unknown_value by default is np.nan as it is said in
+        https://scikit-learn.org/stable/modules/generated/sklearn.preprocessing.OneHotEncoder.html
+        """
+        if 'unknown_value' in self._model_kwargs.keys():
+            unknown_value = self._model_kwargs['unknown_value']
+        else:
+            unknown_value = np.nan
+        transf_values = np.repeat(unknown_value, x.shape[0]).reshape(x.shape[0], 1)
+        transf_values[x[feature].notna()] = self._encoders[feature].transform(x.loc[x[feature].notna(), [feature]])
+        return transf_values
