aa9d4ca7a521e33598d6edd20c6eb4b1f0cdc1ca
AlmudenaZhou <almudena.zhou.rl@gmail.com>
2023-04-09 02:31:51 +0200

    [REFACTOR] Renamed import_module_from_name to instance_class_from_module_and_name function in misc

--- misc.py
+++ misc.py
@@ -1,7 +1,7 @@
 import importlib
 
 
-def import_module_from_name(module_name, class_name, **class_kwargs):
+def instance_class_from_module_and_name(module_name, class_name, **class_kwargs):
     module = importlib.import_module(module_name)
     class_ = getattr(module, class_name)
     instance = class_(**class_kwargs)
