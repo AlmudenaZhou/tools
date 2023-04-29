b3c70d084c41869b770be7ad919379229c183db6
AlmudenaZhou <almudena.zhou.rl@gmail.com>
2023-04-30 00:18:20 +0200

    [FEATURE] Added MinMaxScaler

--- models/transformers/scalers.py
+++ models/transformers/scalers.py
@@ -1,4 +1,4 @@
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
 
 from tools.models.transformers.base import TargetedTransformer
 
@@ -7,3 +7,9 @@ class StandardScalerTransformer(TargetedTransformer):
 
     def _individual_fit(self, x, feature):
         return StandardScaler().fit(x[[feature]])
+
+
+class MinMaxScalerTransformer(TargetedTransformer):
+
+    def _individual_fit(self, x, feature):
+        return MinMaxScaler().fit(x[[feature]])
