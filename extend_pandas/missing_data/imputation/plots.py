9cbb1a146e17fb69bb38576cce973019e4ee0fb5
AlmudenaZhou <almudena.zhou.rl@gmail.com>
2023-05-03 12:01:29 +0200

    [FIX] Fixed distribution_comparison_plot method

--- extend_pandas/missing_data/imputation/plots.py
+++ extend_pandas/missing_data/imputation/plots.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from matplotlib import pyplot as plt
 import seaborn as sns
 
@@ -9,9 +10,10 @@ class PlotImputerAdapter(MissingMethodsAdapter):
     def distribution_comparison_plot(self, imputer, column_to_compare, figsize=(16, 8), **plt_kwargs):
         _, _ = plt.subplots(figsize=figsize)
 
-        sns.kdeplot(self._obj.SkinThickness, label="Original Distribution", **plt_kwargs)
+        sns.kdeplot(self._obj[column_to_compare], label="Original Distribution", **plt_kwargs)
         for model_name, model in imputer._models.items():
             data_imputed = model.transform(self._obj)
+            data_imputed = pd.DataFrame(data_imputed, index=self._obj.index, columns=self._obj.columns)
             sns.kdeplot(data_imputed[column_to_compare], label=f'model: {model_name}', **plt_kwargs)
 
         plt.legend()
